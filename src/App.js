8a4a0247641ac214dcba310193c4609562a1bd5d
Vabalabadabdab <110040060+Deadusik@users.noreply.github.com>
2022-11-26 03:31:20 +0200

    initialize file structure
    add imgs, reset style, initialize components, add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navbar from "./components/navbar/Navbar";
+
+
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/ui/text/Text.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Text = () => {
+    return (
+        <p></p>
+    )
+}
+
+export default Text;
