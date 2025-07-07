cae70ae185a49e3f6189d7909b1565e273f336ef
Ruslan <110040060+Deadusik@users.noreply.github.com>
2025-07-07 23:45:09 +0300

    set hesh router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/reset.scss'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/befa-art'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
